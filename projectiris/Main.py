5dd71b33b586e45e4f1751fc0dc24bc16ef4bb03
David Micciche <gurafity@gmail.com>
2016-12-17 13:00:04 +0100

    Update Main.py

--- projectiris/Main.py
+++ projectiris/Main.py
@@ -1,81 +1,54 @@
 import cv2
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
 import utils
-import numpy as np
+import numpy
+import glob
+import Image
+from itertools import izip
 
-## Read Image
-image2 = cv2.imread('S1001R02.jpg')
-image = cv2.imread('iris3.jpg')
+imageFolderPath = '/home/faust/PycharmProjects/T3032-traitement-de-signal-projet-iris'
+imagePath = glob.glob('*.JPG')
+im_array = numpy.array( [numpy.array(Image.open(imagePath[i]).convert('L'), 'f') for i in range(len(imagePath))] )
 
-imageBackup = image.copy()
+kernel = numpy.ones((5,5),numpy.uint8)
 
-## Convert to 1 channel only grayscale image
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-## CLAHE Equalization
+origin = cv2.imread('img1.JPG')
 
+valueComp = []
+valueMSE = []
 
+res = []
+i = 0
+for picture in glob.glob('*.JPG') :
+    im_cv = cv2.imread(picture)
+    imOrigin = im_cv.copy()
 
-cl1 = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-clahe = cl1.apply(gray)
-## medianBlur the image to remove noise
-medianblur = cv2.medianBlur(clahe, 19)
-blur = cv2.medianBlur(medianblur, 19)
+    crop_array = utils.locateIris(im_cv, kernel)
 
+    res.append(utils.cropIris(imOrigin, crop_array['x1'],crop_array['y1'],crop_array['x2'],crop_array['y2']))
 
+    i1_np = utils.cropIris(origin, crop_array['x1'],crop_array['y1'],crop_array['x2'],crop_array['y2'])
+    i1 = Image.fromarray(i1_np, 'RGB')
 
-edges = cv2.Canny(blur, 30, 60)
+    valueComp.append(utils.compareImages(i1, Image.fromarray(res[i], 'RGB')))
+    valueMSE.append(utils.mse(i1_np, res[i]))
 
-## Detect Circlesr
-circles = cv2.HoughCircles(edges , cv2.cv.CV_HOUGH_GRADIENT,1,3000,
-                            param1=30,param2=15,minRadius=50,maxRadius=200)
+    if min(valueComp) >= valueComp[-1] :
+        pictComp = res[i]
+    if min(valueMSE) >= valueMSE[-1] :
+        pictMSE = res[i]
 
-for circle in circles[0,:]:
-    # draw the outer circle
-    cv2.circle(image,(circle[0],circle[1]),circle[2],(0,255,0),2)
+    i+= 1
 
-    # draw the center of the circle
-    cv2.circle(image,(circle[0],circle[1]),2,(0,0,255),3)
-    print int(circle[0])
-    print int(circle[1])
-    print int(circle[2])
-    x1 = int(circle[0] - circle[2])
-    y1 = int(circle[1] - circle[2])
-    x2 = int(circle[0] + circle[2])
-    y2 = int(circle[1] + circle[2])
+#print min(value)
 
-    crop = imageBackup[y1:y2,x1:x2]
-    #if crop is not None:
-        #cv2.imshow('test', crop)
+cv2.imshow("result", pictComp)
+cv2.imshow("resultMSE", pictMSE)
+print (utils.compareImages(i1, Image.fromarray(pictComp, 'RGB')))
+print (utils.mse(i1_np, pictMSE))
+cv2.imshow("origin", utils.cropIris(origin, crop_array['x1'],crop_array['y1'],crop_array['x2'],crop_array['y2']))
 
-# load the image and convert it from BGR to RGB so that
-# we can dispaly it with matplotlib
-
-image = cv2.cvtColor(crop, cv2.COLOR_BGR2RGB)
-
-# show our image
-plt.figure()
-plt.axis("off")
-plt.imshow(crop)
-
-# reshape the image to be a list of pixels
-image = image.reshape((image.shape[0] * image.shape[1], 3))
-
-# cluster the pixel intensities
-clt = KMeans(10)
-clt.fit(image)
-
-# build a histogram of clusters and then create a figure
-# representing the number of pixels labeled to each color
-hist = utils.centroid_histogram(clt)
-bar = utils.plot_colors(hist, clt.cluster_centers_)
-
-# show our color bart
-plt.figure()
-plt.axis("off")
-plt.imshow(bar)
-plt.show()
-
+############################cv2.inRange()
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
