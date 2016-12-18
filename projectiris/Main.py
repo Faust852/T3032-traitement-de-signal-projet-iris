d752f796a0808b197a39f03283d802f94ee3cf9a
David Micciche <gurafity@gmail.com>
2016-12-18 18:53:20 +0100

    Update Main.py

--- projectiris/Main.py
+++ projectiris/Main.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-import cv
 import Image
 import utils
 from matplotlib import pyplot as plt
@@ -11,8 +10,8 @@ from sklearn.preprocessing import normalize
 
 kernel = np.ones((3,3),np.uint8)
 
-path = './images/NIR_2/001_2_2.bmp'
-path2 = './images/NIR_2/001_1_3.bmp'
+path = './images/NIR_2/001_1_2.bmp'
+path2 = './images/NIR_2/001_2_3.bmp'
 
 img = utils.locateIris(path)
 img2 = utils.locateIris(path2)
@@ -22,21 +21,17 @@ processed_img2 = utils.irisProcessing(img2['im'], kernel)
 
 cv2.imwrite("tmp1.jpg", img["im"])
 temp1 = cv2.imread("tmp1.jpg")
-cv2.imshow("tmp", temp1)
 
 cv2.imwrite("tmp2.jpg", img2["im"])
 temp2 = cv2.imread("tmp2.jpg")
-cv2.imshow("tmp2", temp2)
 
-test = utils.normalize(cv.fromarray(temp1),int(img["rad_iris"]))
+test = utils.normalize(temp1,int(img["rad_iris"]))
 
-test2 = utils.normalize(cv.fromarray(temp2),int(img2["rad_iris"]))
+test2 = utils.normalize(temp2,int(img2["rad_iris"]))
 
 var = np.asarray(test[:,:] )
-cv2.imshow("test", var)
 
 var2 = np.asarray(test2[:,:] )
-cv2.imshow("test2", var2)
 
 cv2.imwrite("tmpthresh.jpg", var)
 cv2.imwrite("tmpthresh2.jpg", var2)
@@ -44,23 +39,20 @@ cv2.imwrite("tmpthresh2.jpg", var2)
 im = cv2.imread("tmpthresh.jpg")
 im2 = cv2.imread("tmpthresh2.jpg")
 
-# imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-# ret,thresh = cv2.threshold(imgray,1,255,0)
-# opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-# contours, hierarchy = cv2.findContours(opening,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-#
-# cv2.drawContours(im, contours, -1, (0,255,0), 1)
-#
-# cv2.imshow("tsk",im)
+im = ndimage.rotate(im, 90)
+im2 = ndimage.rotate(im2, 90)
 
-# test2 = utils.normalize(cv.fromarray(temp2),int(img2['rad_iris']))
-# cv2.imshow("test2", np.asarray(test2[:,:] ))
-
-pattern = utils.findPatern(im)
-pattern2 = utils.findPatern(im2)
+im = utils.irisProcessing(im, kernel)
+im2 = utils.irisProcessing(im2, kernel)
+cv2.imshow("im", im)
+cv2.imshow("im2", im2)
 
 
+# pattern = utils.findPattern(im_clahe)
+# pattern2 = utils.findPattern(im_clahe2)
 
+
+utils.comparePattern(im, im2)
 
 
 
@@ -86,10 +78,10 @@ cv2.waitKey(0)
 # min_index[mip] = 1
 # min_feature = min_index[labeled_array]
 
-plt.subplot(121)
-plt.imshow(pattern)
-plt.subplot(122)
-plt.imshow(pattern2)
-plt.show()
+# plt.subplot(121)
+# plt.imshow(thresh1)
+# plt.subplot(122)
+# plt.imshow(thresh2)
+# plt.show()
 
 cv2.waitKey(0)
