0bb73b636baea8b04bc2d7d9058d434434eff519
David Micciche <gurafity@gmail.com>
2016-12-19 14:58:36 +0100

    Update Main.py

--- projectiris/Main.py
+++ projectiris/Main.py
@@ -3,15 +3,17 @@ import numpy as np
 import cv2
 import utils
 
-i1,s1,n1,b1= utils.wrapperIris('./images/NIR_2/002_1_1.bmp') #OEIL 1
-i2,s2,n2,b2 = utils.wrapperIris('./images/NIR_2/003_2_3.bmp') #OEIL 2
 
+i1,s1,n1,b1= utils.wrapperIris('./images/NIR_2/003_2_1.bmp')
+i2,s2,n2,b2 = utils.wrapperIris('./images/NIR_2/003_1_2.bmp')
+
+v0 = utils.comparePattern(i1['im'], i2['im'])
 v1 = utils.comparePattern(s1, s2)
 v2 = utils.comparePattern(n1, n2)
 v3 = utils.comparePattern(b1, b2)
 v4 = utils.binaryComparison(b1,b2)
 print v4
-if ((v1 > 150) & (v2 > 30) & (v3 > 10)) :
+if ((v1 > 150) & (v2 > 50) & (v3 > 20)) :
     print "MATCHING"
 else:
     print "NOT MATCHING"
