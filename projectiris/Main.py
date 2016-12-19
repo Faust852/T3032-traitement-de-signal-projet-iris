e11ee3c82ab8d8e3cdc287aecd2c1b961b4c50df
David Micciche <gurafity@gmail.com>
2016-12-19 12:56:33 +0100

    Update Main.py

--- projectiris/Main.py
+++ projectiris/Main.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import cv2
 import Image
@@ -8,80 +9,27 @@ from itertools import izip
 from scipy import ndimage
 from sklearn.preprocessing import normalize
 
-kernel = np.ones((3,3),np.uint8)
+kernel = np.ones((5,5),np.uint8)
 
-path = './images/NIR_2/001_1_2.bmp'
-path2 = './images/NIR_2/001_2_3.bmp'
+im1 = cv2.imread('./images/NIR_2/001_1_1.bmp')
+im2 = cv2.imread('./images/NIR_2/001_2_2.bmp')
 
-img = utils.locateIris(path)
-img2 = utils.locateIris(path2)
+im1 = utils.locateIris('./images/NIR_2/001_1_1.bmp')
+im2 = utils.locateIris('./images/NIR_2/001_1_2.bmp')
+im1_smg = utils.irisProcessing(im1['im'], kernel)
+im2_smg = utils.irisProcessing(im2['im'], kernel)
 
-processed_img = utils.irisProcessing(img['im'], kernel)
-processed_img2 = utils.irisProcessing(img2['im'], kernel)
+im1_norm = utils.normalize(im1_smg, im1['rad_iris'])
+im2_norm = utils.normalize(im2_smg, im2['rad_iris'])
 
-cv2.imwrite("tmp1.jpg", img["im"])
-temp1 = cv2.imread("tmp1.jpg")
+_,im1_bina = cv2.threshold(im1_norm, 190, 255, cv2.THRESH_BINARY)
+_,im2_bina = cv2.threshold(im2_norm, 190, 255, cv2.THRESH_BINARY)
 
-cv2.imwrite("tmp2.jpg", img2["im"])
-temp2 = cv2.imread("tmp2.jpg")
-
-test = utils.normalize(temp1,int(img["rad_iris"]))
-
-test2 = utils.normalize(temp2,int(img2["rad_iris"]))
-
-var = np.asarray(test[:,:] )
-
-var2 = np.asarray(test2[:,:] )
-
-cv2.imwrite("tmpthresh.jpg", var)
-cv2.imwrite("tmpthresh2.jpg", var2)
-
-im = cv2.imread("tmpthresh.jpg")
-im2 = cv2.imread("tmpthresh2.jpg")
-
-im = ndimage.rotate(im, 90)
-im2 = ndimage.rotate(im2, 90)
-
-im = utils.irisProcessing(im, kernel)
-im2 = utils.irisProcessing(im2, kernel)
-cv2.imshow("im", im)
-cv2.imshow("im2", im2)
-
-
-# pattern = utils.findPattern(im_clahe)
-# pattern2 = utils.findPattern(im_clahe2)
-
-
-utils.comparePattern(im, im2)
-
-
+cv2.imshow('ima',im1['im'])
+cv2.imshow('imb',im2['im'])
+cv2.imshow('im',im1_norm)
+cv2.imshow('im2',im2_norm)
 
 
 cv2.waitKey(0)
-
-# image_threshold = .5
-#
-# s = ndimage.generate_binary_structure(2,2)
-#
-# labeled_array, num_features = ndimage.label(processed_img, structure=s)
-# print(num_features)
-#
-# sizes = ndimage.sum(processed_img,labeled_array,range(1,num_features+1))
-# map = np.where(sizes==sizes.max())[0] + 1
-# mip = np.where(sizes==sizes.min())[0] + 1
-#
-# max_index = np.zeros(num_features + 1, np.uint8)
-# max_index[map] = 1
-# max_feature = max_index[labeled_array]
-#
-# min_index = np.zeros(num_features + 1, np.uint8)
-# min_index[mip] = 1
-# min_feature = min_index[labeled_array]
-
-# plt.subplot(121)
-# plt.imshow(thresh1)
-# plt.subplot(122)
-# plt.imshow(thresh2)
-# plt.show()
-
-cv2.waitKey(0)
+cv2.destroyAllWindows()
