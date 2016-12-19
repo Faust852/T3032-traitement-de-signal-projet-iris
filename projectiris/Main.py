bbfa9f30bd9f3d3e3dc3afe82d6d2dc438d9d3ab
David Micciche <gurafity@gmail.com>
2016-12-19 21:09:02 +0100

    Update Main.py

--- projectiris/Main.py
+++ projectiris/Main.py
@@ -1,19 +1,22 @@
 
-import numpy as np
+import numpy
 import cv2
 import utils
+import Image
+import glob
+import argparse
+from scipy import ndimage
+from matplotlib import pyplot as plt
+
+i1,s1,n1,b1= utils.wrapperIris('./images/NIR_2/002_1_3.bmp')
+i2,s2,n2,b2 = utils.wrapperIris('./images/NIR_2/001_1_2.bmp')
 
 
-i1,s1,n1,b1= utils.wrapperIris('./images/NIR_2/003_2_1.bmp')
-i2,s2,n2,b2 = utils.wrapperIris('./images/NIR_2/003_1_2.bmp')
-
-v0 = utils.comparePattern(i1['im'], i2['im'])
 v1 = utils.comparePattern(s1, s2)
-v2 = utils.comparePattern(n1, n2)
-v3 = utils.comparePattern(b1, b2)
-v4 = utils.binaryComparison(b1,b2)
-print v4
-if ((v1 > 150) & (v2 > 50) & (v3 > 20)) :
+utils.comparePattern(n1, n2)
+if (v1 > 199) :
     print "MATCHING"
 else:
     print "NOT MATCHING"
+
+cv2.waitKey(0)
